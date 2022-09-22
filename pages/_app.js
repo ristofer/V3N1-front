a271cbf855daa0fa7058783a0d370a9c321316aa
ignacio <iaheck@uc.cl>
2022-09-22 14:58:46 -0300

    feat(openAPI): offers OpenAPIProvider to all components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { OpenAPIProvider } from 'react-openapi-client';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <OpenAPIProvider definition="http://localhost:3000/api/v1/swagger.json">
+      <Component {...pageProps} />
+    </OpenAPIProvider>
+  )
 }
 
 export default MyApp
