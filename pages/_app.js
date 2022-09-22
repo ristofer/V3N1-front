525d330bbfc9cc266ae5e39ad81eee04dbc77b0b
ignacio <iaheck@uc.cl>
2022-09-22 17:52:10 -0300

    feat(openAPI): uses relative URL so we don't need env variable

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { OpenAPIProvider } from 'react-openapi-client';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <OpenAPIProvider definition="http://localhost:3001/api-docs/v1/swagger.json">
+    <OpenAPIProvider definition="/api-docs/v1/swagger.json">
       <Component {...pageProps} />
     </OpenAPIProvider>
   )
