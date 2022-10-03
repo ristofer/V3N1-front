442e13e9c2c1c49b776390f6c524301cc7d7a1b0
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-03 14:49:09 -0300

    fix: routes

--- pages/resources/[resourceId].js
+++ pages/resources/[resourceId].js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import NewResourceComment from "../components/NewResourceComment";
+import NewResourceComment from "../../components/NewResourceComment";
 
 export default function Resources() {
   const router = useRouter();
