e20efd928a54b21173a09602b44920f137b2bb0b
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-03 23:46:35 -0300

    refactor: removed unnecessary openAPI tags

--- pages/curriculums/[curriculumId].js
+++ pages/curriculums/[curriculumId].js
@@ -1,17 +1,14 @@
 import React from "react";
-import { OpenAPIProvider } from "react-openapi-client";
 import LearningUnitCheckbox from "../../components/LearningUnitCheckbox";
 
 export default function LearningUnits() {
   const isCompleted = false;
   return (
     <React.StrictMode>
-      <OpenAPIProvider definition="/api-docs/v1/swagger.json">
-        <LearningUnitCheckbox
-          learningUnitId={1}
-          isPreviouslyCompleted={isCompleted}
-        />
-      </OpenAPIProvider>
+      <LearningUnitCheckbox
+        learningUnitId={1}
+        isPreviouslyCompleted={isCompleted}
+      />
     </React.StrictMode>
   );
 }
