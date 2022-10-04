c560430af26280fb4c299a632fcf9ca2a1bce9eb
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-04 15:41:46 -0300

    refactor: moved components to 'resources' folder

--- pages/learningUnits/[learningUnitId].js
+++ pages/learningUnits/[learningUnitId].js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import NewResource from "../../components/NewResource";
+import NewResource from "../../components/resources/NewResource";
 
 export default function LearningUnits() {
   const router = useRouter();
