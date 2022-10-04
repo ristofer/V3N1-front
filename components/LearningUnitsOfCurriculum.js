d8dd16bb9dfd004f56daafd9f19d13b60a110ba8
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-04 14:38:03 -0300

    feat: component to list all learning units

---
+++ components/LearningUnitsOfCurriculum.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useOperation } from "react-openapi-client";
+import { Alert, Container } from "@mui/material";
+import Loader from "./Loader";
+import LearningUnit from "./LearningUnit";
+
+export default function LearningUnitsOfCurriculum({ curriculumId }) {
+  const {
+    loading: completedLoading,
+    error: completedError,
+    data: completedData,
+  } = useOperation(
+    "listCompletedLearningUnitsOfCurriculumCompletedByUser",
+    curriculumId
+  );
+
+  const {
+    loading: learningUnitsLoading,
+    data: learningUnitsData,
+    error: learningUnitsError,
+  } = useOperation("listLearningUnitsOfCurriculum", curriculumId);
+
+  // const isCompleted = false;
+
+  if (learningUnitsError || completedError) {
+    return <Alert severity="error">Error</Alert>;
+  }
+  if (learningUnitsLoading || completedLoading) {
+    return <Loader />;
+  }
+
+  const completedLearningUnits = completedData.map(
+    (completedLearningUnit) => completedLearningUnit.learning_unit_id
+  );
+
+  return (
+    <Container maxWidth="sm">
+      {learningUnitsData.map((learningUnit) => (
+        <LearningUnit
+          learningUnit={learningUnit}
+          isPreviouslyCompleted={completedLearningUnits.includes(
+            learningUnit.id
+          )}
+        />
+      ))}
+    </Container>
+  );
+}
