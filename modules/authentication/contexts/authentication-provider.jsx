3c682fca98a56ebe204650c321217107cf2d2f7f
ignacio <iaheck@uc.cl>
2022-10-04 18:40:55 -0300

    style(authenticationProvider): adds useCallback and useMemo to context

--- modules/authentication/contexts/authentication-provider.jsx
+++ modules/authentication/contexts/authentication-provider.jsx
@@ -1,29 +1,30 @@
 import { useOperation, useOperationMethod } from "react-openapi-client";
 import axios from "axios";
-
+import { useMemo, useCallback } from "react";
 import AuthenticationContext from "./authentication-context";
 
 function AuthenticationProvider({ children }) {
   const [endSession] = useOperationMethod("endSession");
   const { loading, data, error } = useOperation("getSession");
 
-  const HandleSignOut = async () => {
+  const signOut = useCallback(async () => {
     await endSession();
     if (window.location.pathname === "/") {
       window.location.reload();
     } else {
       axios.get("/");
     }
-  };
+  }, [endSession]);
 
-  const context = {
-    currentUser: data,
-    loading,
-    error,
-    signOut: async () => {
-      HandleSignOut();
-    },
-  };
+  const context = useMemo(
+    () => ({
+      currentUser: data,
+      loading,
+      error,
+      signOut,
+    }),
+    [data, error, loading, signOut]
+  );
 
   return (
     <AuthenticationContext.Provider value={context}>
