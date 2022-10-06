243050850566fb70974812f91819f945b2b00096
ignacio <iaheck@uc.cl>
2022-10-06 18:24:01 -0300

    feat(navbar): adds Link to navbar

--- components/navbar/AppTitleAndLogo.js
+++ components/navbar/AppTitleAndLogo.js
@@ -1,28 +1,30 @@
 import SchoolIcon from "@mui/icons-material/School";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 function AppTitleAndLogo({ display, variant, flexGrow }) {
   return (
     <>
       <SchoolIcon sx={{ display, mr: 1 }} />
-      <Typography
-        variant={variant}
-        noWrap
-        component="a"
-        href="/"
-        sx={{
-          mr: 2,
-          display,
-          flexGrow,
-          fontFamily: "monospace",
-          fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
-          textDecoration: "none",
-        }}
-      >
-        V3N1
-      </Typography>
+      <Link href="/" passHref>
+        <Typography
+          variant={variant}
+          noWrap
+          component="a"
+          sx={{
+            mr: 2,
+            display,
+            flexGrow,
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          V3N1
+        </Typography>
+      </Link>
     </>
   );
 }
