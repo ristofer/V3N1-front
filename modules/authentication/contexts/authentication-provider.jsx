951212cab0fc6379ffc2e83cb3001da7ce9bb012
ignacio <iaheck@uc.cl>
2022-10-05 22:22:48 -0300

    feat(landing): routes only if current route is not landing and endsession only if it's open

--- modules/authentication/contexts/authentication-provider.jsx
+++ modules/authentication/contexts/authentication-provider.jsx
@@ -1,17 +1,22 @@
 import { useOperationMethod } from "react-openapi-client";
 import { useMemo, useCallback } from "react";
-import Router from "next/router";
+import { useRouter } from "next/router";
 import AuthenticationContext from "./authentication-context";
 import useFetch from "../../../hooks/use-fetch";
 
 function AuthenticationProvider({ children }) {
   const [endSession] = useOperationMethod("endSession");
+  const router = useRouter();
 
   const { data, error } = useFetch("/api/current_session");
 
   const signOut = useCallback(async () => {
-    await endSession();
-    await Router.push("/landing/");
+    if (error === undefined) {
+      await endSession();
+    }
+    if (router.pathname !== "/landing") {
+      await router.push("/landing");
+    }
     window.location.reload();
   }, [endSession]);
 
