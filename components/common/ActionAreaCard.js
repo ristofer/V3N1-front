1313d38fc35f58deb545f8637796b3e5bd7498bc
ignacio <iaheck@uc.cl>
2022-10-06 18:09:32 -0300

    feat(home): uses Link to strongly improve loading performance

--- components/common/ActionAreaCard.js
+++ components/common/ActionAreaCard.js
@@ -5,26 +5,29 @@ import {
   Typography,
   CardActionArea,
 } from "@mui/material";
+import Link from "next/Link";
 
 function ActionAreaCard({ imageurl, url, imagealt, title, text }) {
   return (
     <Card sx={{ maxWidth: 345, m: 2 }}>
-      <CardActionArea href={url}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={imageurl}
-          alt={imagealt}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {text}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link href={url}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={imageurl}
+            alt={imagealt}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {text}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </Card>
   );
 }
