a410bbd4fb74dc1fcca0ec5bc1333d17337c1c5a
ristofer <cristopher.gomez@ug.uchile.cl>
2022-10-06 01:53:12 -0300

    feat: add method to change the resource evaluation to the ResourceInformation component

--- components/resources/ResourceInformation.js
+++ components/resources/ResourceInformation.js
@@ -1,4 +1,5 @@
 import { Alert } from "@mui/material";
+import { useOperationMethod } from "react-openapi-client";
 import useFetch from "../../hooks/use-fetch";
 import Loader from "../common/Loader";
 import ResourceDisplay from "./ResourceDisplay";
@@ -7,9 +8,13 @@ import UserRating from "./UserRating";
 function ResourceInformation({ resourceId }) {
   const { data, error } = useFetch(`/api/resources/${resourceId}`);
 
-  const { data: evaluationData, error: evaluationError } = useFetch(
-    `/api/resources/${resourceId}/resource_evaluation`
-  );
+  const {
+    data: evaluationData,
+    error: evaluationError,
+    mutate,
+  } = useFetch(`/api/resources/${resourceId}/resource_evaluation`);
+
+  const [setResourceEvaluation] = useOperationMethod("setResourceEvaluation");
 
   if (error || evaluationError) return <Alert severity="error">Error</Alert>;
   if (!data || !evaluationData) return <Loader />;
@@ -23,7 +28,13 @@ function ResourceInformation({ resourceId }) {
   return (
     <>
       <ResourceDisplay resource={data} />
-      <UserRating evaluation={userEvaluation} />
+      <UserRating
+        evaluation={userEvaluation}
+        setEvaluation={(body) => {
+          setResourceEvaluation(resourceId, body);
+          mutate();
+        }}
+      />
     </>
   );
 }
