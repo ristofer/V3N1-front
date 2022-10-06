63b92255925cf0f888bdc4216acdfd112cbf8aba
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-06 18:45:05 -0300

    refactor: CommentSection queries the backend

--- components/resources/CommentSection.jsx
+++ components/resources/CommentSection.jsx
@@ -1,10 +1,13 @@
 import { Alert, Container } from "@mui/material";
+import { useOperationMethod } from "react-openapi-client";
 import Loader from "../common/Loader";
 import Comment from "./Comment";
 import NewResourceComment from "./NewResourceComment";
 import useFetch from "../../hooks/use-fetch";
 
 function CommentSection({ resourceId }) {
+  const [createResourceComment] = useOperationMethod("createResourceComment");
+
   const { data, error, mutate } = useFetch(
     `/api/resources/${resourceId}/resource_comments`
   );
@@ -12,7 +15,10 @@ function CommentSection({ resourceId }) {
   if (error) return <Alert severity="error">Error</Alert>;
   if (!data) return <Loader />;
 
-  const newCommentCreation = () => {
+  const onSubmit = async (content) => {
+    await createResourceComment(resourceId, {
+      content,
+    });
     mutate();
   };
 
@@ -21,10 +27,7 @@ function CommentSection({ resourceId }) {
       {data.map((comment) => (
         <Comment key={comment.id} comment={comment} />
       ))}
-      <NewResourceComment
-        resourceId={resourceId}
-        newCommentCreation={newCommentCreation}
-      />
+      <NewResourceComment onSubmit={onSubmit} />
     </Container>
   );
 }
