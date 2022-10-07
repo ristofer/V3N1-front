848efd063c25532391931c030c33e389f2253f05
ignacio <iaheck@uc.cl>
2022-10-07 10:56:09 -0300

    style(curriculum-style): removes unused dependecies and changes subtitle style

--- components/curriculums/CurriculumInformation.js
+++ components/curriculums/CurriculumInformation.js
@@ -1,4 +1,4 @@
-import { Typography, Alert, Card, CardContent } from "@mui/material";
+import { Typography, Alert } from "@mui/material";
 import Loader from "../common/Loader";
 import useFetch from "../../hooks/use-fetch";
 
@@ -17,7 +17,7 @@ export default function CurriculumInformation({ curriculumId }) {
 
   return (
     <div style={{ maxWidth: 700, margin: "40px auto", padding: 10 }}>
-      <Typography variant="h5" align="center" color="textSecondary">
+      <Typography variant="subtitle1" align="center" color="textSecondary">
         <i>Curriculum</i>
       </Typography>
       <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
