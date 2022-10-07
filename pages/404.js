dfc5e9756ce0026148bb7dd6f3e93b98b543f58f
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-07 12:38:46 -0300

    simple 404 error page

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import { Typography, Grid } from "@mui/material";
+
+export default function Custom404() {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "90vh" }}
+    >
+      <Typography variant="h3">
+        Hmmm... this page doesn't seem to exist
+      </Typography>
+      <Typography variant="h6">(Error 404)</Typography>
+    </Grid>
+  );
+}
