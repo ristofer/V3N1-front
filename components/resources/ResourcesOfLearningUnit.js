5b5677ed8a081468a260067bc3989d31e8b8fd3a
Juan Santos <jjsantosochoa@outlook.com>
2022-10-07 16:31:57 -0300

    refactor(learningunits_page): drop newResource button from ResourcesOflearningUnit

--- components/resources/ResourcesOfLearningUnit.js
+++ components/resources/ResourcesOfLearningUnit.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Alert, Container } from "@mui/material";
-import { useOperationMethod } from "react-openapi-client";
 import useFetch from "../../hooks/use-fetch";
 import Loader from "../common/Loader";
 import Resource from "./Resource";
-import NewResource from "./NewResource";
 
 export default function ResourcesOfLearningUnit({ learningUnitId }) {
-  const [createResource] = useOperationMethod("createResource");
-  const { data, error, mutate } = useFetch(
+  const { data, error } = useFetch(
     `/api/learning_units/${learningUnitId}/resources`
   );
 
@@ -16,21 +13,11 @@ export default function ResourcesOfLearningUnit({ learningUnitId }) {
   if (!data) return <Loader />;
   data.sort((a, b) => (b.average_evaluation > a.average_evaluation ? 1 : -1));
 
-  const onSubmit = async (name, url, description) => {
-    await createResource(learningUnitId, {
-      name,
-      url,
-      description,
-    });
-    mutate();
-  };
-
   return (
     <Container maxWidth="xl" mt={2}>
       {data.map((resource) => (
         <Resource key={resource.id} resource={resource} />
       ))}
-      <NewResource onSubmit={onSubmit} />
     </Container>
   );
 }
